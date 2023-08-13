812cfedfa0acfaca3988b718fc6098496f240dcf
black-adm <black.adm7@gmail.com>
2023-08-13 16:19:11 -0300

    Lógica para criar um usuário

---
+++ src/usecases/users/createUser.ts
@@ -0,0 +1,24 @@
+import { User } from "@prisma/client";
+import { prisma } from "../../database/client";
+import { CreateUserDto } from "../../dtos/createUserDto";
+
+export const CreateUser = async ({ name, email }: CreateUserDto): Promise<User> => {
+    const userAlreadyExists = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (userAlreadyExists) {
+        throw new Error("E-mail já cadastrado!")
+    }
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email
+        }
+    });
+
+    return user;
+}
